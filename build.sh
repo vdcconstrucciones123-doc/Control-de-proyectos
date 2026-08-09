73491f8fab75810c66d9b023f79dcd9983aee6c4
VDCCONTROLDEPROYECTOS <vdcconstrucciones123@gmail.com>
2026-08-08 21:56:00 -0500

    Add persistence, admin bootstrap, and Render deploy fixes

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
+export PLAYWRIGHT_BROWSERS_PATH="/opt/render/project/.playwright"
+
 pip install -r requirements.txt
 python -m playwright install chromium
 python manage.py collectstatic --no-input
